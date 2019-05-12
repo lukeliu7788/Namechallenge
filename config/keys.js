19e70b7aa70fe89836d6c8dec03f3bd3d4a4647d
Luke <lukeliu7788@gmail.com>
2019-05-12 21:30:43 +1000

    Add Keys

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//ClientID is public key
+//ClientSecret is private key
+
+module.exports={
+    mongoURI: 'mongodb+srv://lukeliu:4Q5f-u_DhJ_!HmM@cluster0-ynbu6.mongodb.net/test?retryWrites=true',
+    mongoUserPassWord:'4Q5f-u_DhJ_!HmM',
+};
+
